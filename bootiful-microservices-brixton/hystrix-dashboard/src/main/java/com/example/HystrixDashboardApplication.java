b6660448b7f9f0accace52d628e1dfd488b92640
Josh Long <josh@joshlong.com>
2015-12-18 10:24:35 -0500

    adding hystrix-dashboard

---
+++ bootiful-microservices-brixton/hystrix-dashboard/src/main/java/com/example/HystrixDashboardApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableHystrixDashboard
+@SpringBootApplication
+public class HystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+}
