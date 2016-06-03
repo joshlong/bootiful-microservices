9287d278c9b5bf0ce8328073a45b061db6128931
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-06-03 11:04:35 +0200

    Trying to fix Jenkins

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -4,5 +4,7 @@ source common.sh || source scripts/common.sh || echo "No common.sh script found.
 
 set -e
 
+cd $ROOT_FOLDER
+
 # Packages all apps in parallel using 6 cores
 ./mvnw clean package -T 6
