bca64675e92a84821b493dd132bb1cacf4b43d62
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-07-07 13:58:13 +0200

    Clearing up the amount of logs during build

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -13,4 +13,4 @@ cd $ROOT_FOLDER
 echo -e "\nRunning the build with additional options [$ADDITIONAL_MAVEN_OPTS] and profile [$PROFILE]"
 
 # Packages all apps in parallel using 6 cores
-./mvnw clean package -T 6 $ADDITIONAL_MAVEN_OPTS -P$PROFILE
+./mvnw clean package -T 6 $ADDITIONAL_MAVEN_OPTS -P$PROFILE -U --batch-mode -Dmaven.test.redirectTestOutputToFile=true
