919cb9f964655649133ce781b3b61e6236bad5f7
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-07-07 13:24:38 +0200

    Added test_all script

---
+++ scripts/test_all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+source common.sh || source scripts/common.sh || echo "No common.sh script found..."
+
+set -e
+
+echo -e "This script will run Brixton and then Camden tests"
+
+./scenario_brixton_tester.sh || ./scripts/scenario_brixton_tester.sh
+./scenario_camden_tester.sh || ./scripts/scenario_camden_tester.sh
